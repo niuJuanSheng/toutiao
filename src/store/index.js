1fdb3dee263a05ad5e737e3a853a5e25ad5d4148
liuBoMo <liubomo777@126.com>
2021-09-23 20:31:52 +0800

    feat：建立模块管理分类和新闻数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import category from './modules/category'
+import newList from './modules/newList'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    category,
+    newList
   }
 })
