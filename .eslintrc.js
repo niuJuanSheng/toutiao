d09351a994c4d1cdfa8630c18243338510a371d7
liuBoMo <liubomo777@126.com>
2021-09-23 20:00:08 +0800

    项目初始化构建

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    'prefer-const': 0, // 关闭 let 转换为 const
+    ' camelcase': 'off',
+    '@typescript-eslint/camelcase': 0
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="catagtory">
+      <li class="select">开发者资讯</li>
+      <li>ios</li>
+      <li>c++</li>
+      <li>android</li>
+      <li>css</li>
+      <li>数据库</li>
+      <li>区块链</li>
+      <li>go</li>
+      <li>产品</li>
+      <li>后端</li>
+      <li>linux</li>
+      <li>人工智能</li>
+      <li>php</li>
+      <li>javascript</li>
+      <li>架构</li>
+      <li>前端</li>
+      <li>python</li>
+      <li>java</li>
+      <li>算法</li>
+      <li>面试</li>
+      <li>科技动态</li>
+      <li>js</li>
+      <li>设计</li>
+      <li>数码产品</li>
+      <li>html</li>
+      <li>软件测试</li>
+      <li>测试开发</li>
+    </ul>
+    <div class="list">
+      <div class="article_item">
+        <h3 class="van-ellipsis">python数据预处理 ：数据标准化</h3>
+        <div class="img_box">
+          <img src="@/assets/back.jpg" class="w100" />
+        </div>
+        <!---->
+        <div class="info_box">
+          <span>13552285417</span>
+          <span>0评论</span>
+          <span>2018-11-29T17:02:09</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped lang="less"></style>
